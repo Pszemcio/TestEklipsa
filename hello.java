3d91250a723a1c54c778ec593934f24020f7ecbb
Pszemcio <73850832+Pszemcio@users.noreply.github.com>
2020-12-21 10:54:48 +0100

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World 2:0.a!");
 		System.out.println("Nowa linijka");
-		System.out.println("I jeszcze jedna. Ha-ha!");
+		System.out.println("I jeszcze jedna. Ha-ha-ha!");
 	}
 }
